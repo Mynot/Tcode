7245be2e864f6e00a5bc948b0bbdf618fd8cf1eb
Mynot <stefan.schweifer@gmail.com>
2016-01-08 22:39:08 +0100

    Sicherung auf PC

--- app/src/main/java/sap/schweifer/at/tcode/ArrayAdapterItem.java
+++ app/src/main/java/sap/schweifer/at/tcode/ArrayAdapterItem.java
@@ -31,10 +31,7 @@ public class ArrayAdapterItem extends ArrayAdapter<CodeObjects> {
     public View getView(int position, View convertView, ViewGroup parent) {
 
     	/*
-		 * The convertView argument is essentially a "ScrapView" as described is Lucas post
-		 * http://lucasr.org/2012/04/05/performance-tips-for-androids-listview/
-		 * It will have a non-null value when ListView is asking you recycle the row layout.
-		 * So, when convertView is not null, you should simply update its contents instead of inflating a new row layout.
+
     	 */
         if(convertView==null){
             // inflate the layout
